2d30d880f53ccd80a7c46f97939b01380d386823
holmestrevor <holmes.trevor98@users.noreply.github.com>
2019-02-26 23:30:49 -0500

    Added fade-in animation to TitleActivity

--- app/src/main/java/com/example/portfolioapp/TitleActivity.java
+++ app/src/main/java/com/example/portfolioapp/TitleActivity.java
@@ -2,6 +2,13 @@ package com.example.portfolioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.view.View.INVISIBLE;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +16,19 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Button button = (Button)findViewById(R.id.titleButton);
+        ImageView img = (ImageView)findViewById(R.id.fadeOut);
+
+        Animation aniFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        Animation aniFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+
+        img.startAnimation(aniFadeOut);
+        img.setVisibility(INVISIBLE);
+        button.startAnimation(aniFadeIn);
+
+
+
     }
 }
