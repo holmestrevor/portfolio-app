93d83446c8ce9172a80aae676493d7b5ff8033b9
holmestrevor <holmes.trevor98@users.noreply.github.com>
2019-02-27 11:52:24 -0500

    Added animations to TitleActivity

--- app/src/main/java/com/example/portfolioapp/TitleActivity.java
+++ app/src/main/java/com/example/portfolioapp/TitleActivity.java
@@ -2,33 +2,55 @@ package com.example.portfolioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import static android.view.View.INVISIBLE;
 
 public class TitleActivity extends AppCompatActivity {
 
+    TextView title;
+    TextView subtitle;
+    Button button;
+    ImageView img;
+    ImageView bg;
+    Animation aniFadeOut;
+    Animation aniFadeIn;
+    Animation shrink;
+    Animation shrinkLess;
+    Animation moveUp;
+    Animation aniFadeInLong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_title);
         getSupportActionBar().hide();
 
-        Button button = (Button)findViewById(R.id.titleButton);
-        ImageView img = (ImageView)findViewById(R.id.fadeOut);
+        title = (TextView)findViewById(R.id.titleText);
+        subtitle = (TextView)findViewById(R.id.subtitleText);
+        button = (Button)findViewById(R.id.titleButton);
+        img = (ImageView)findViewById(R.id.fadeOut);
+        bg = (ImageView)findViewById(R.id.titleBG);
 
-        Animation aniFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
-        Animation aniFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        aniFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        aniFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        aniFadeInLong = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in_long);
+        shrink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shrink);
+        moveUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.moveup);
+        shrinkLess = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shrink_less);
 
+        bg.startAnimation(shrinkLess);
         img.startAnimation(aniFadeOut);
         img.setVisibility(INVISIBLE);
         button.startAnimation(aniFadeIn);
-
-
+        title.startAnimation(aniFadeIn);
+        title.startAnimation(shrink);
+        subtitle.startAnimation(aniFadeInLong);
+        subtitle.startAnimation(moveUp);
 
     }
 }
