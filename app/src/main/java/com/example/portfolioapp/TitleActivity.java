001b818bbddafc53c50eb02f70d63115e2f35ebe
holmestrevor <holmes.trevor98@users.noreply.github.com>
2019-02-26 22:54:03 -0500

    Added TitleActivity

--- app/src/main/java/com/example/portfolioapp/TitleActivity.java
+++ app/src/main/java/com/example/portfolioapp/TitleActivity.java
@@ -3,7 +3,7 @@ package com.example.portfolioapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TitleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
