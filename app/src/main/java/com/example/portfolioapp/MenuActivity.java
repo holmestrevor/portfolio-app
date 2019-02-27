075f113821a6a824277e7b592f830236543eb77a
holmestrevor <holmes.trevor98@gmail.com>
2019-02-27 13:09:13 -0500

    Added delay to button fade-in animation, made the animation shorter.

---
+++ app/src/main/java/com/example/portfolioapp/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.portfolioapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+}
--- app/src/main/java/com/example/portfolioapp/TitleActivity.java
+++ app/src/main/java/com/example/portfolioapp/TitleActivity.java
@@ -1,7 +1,10 @@
 package com.example.portfolioapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -9,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -23,6 +27,7 @@ public class TitleActivity extends AppCompatActivity {
     Animation shrinkLess;
     Animation moveUp;
     Animation aniFadeInLong;
+    Animation aniFadeInShort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,24 +38,51 @@ public class TitleActivity extends AppCompatActivity {
         title = (TextView)findViewById(R.id.titleText);
         subtitle = (TextView)findViewById(R.id.subtitleText);
         button = (Button)findViewById(R.id.titleButton);
+        button.setVisibility(INVISIBLE);
         img = (ImageView)findViewById(R.id.fadeOut);
         bg = (ImageView)findViewById(R.id.titleBG);
 
         aniFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
         aniFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
         aniFadeInLong = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in_long);
+        aniFadeInShort = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in_short);
         shrink = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shrink);
         moveUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.moveup);
         shrinkLess = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shrink_less);
 
+        /*
+        When the moveUp animation has ended, it will call for the button to appear. Otherwise,
+        there would be no delay.
+         */
+        moveUp.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                button.setVisibility(VISIBLE);
+                button.startAnimation(aniFadeInShort);
+            }
+        });
+
         bg.startAnimation(shrinkLess);
         img.startAnimation(aniFadeOut);
         img.setVisibility(INVISIBLE);
-        button.startAnimation(aniFadeIn);
+//        button.startAnimation(aniFadeIn);
         title.startAnimation(aniFadeIn);
         title.startAnimation(shrink);
         subtitle.startAnimation(aniFadeInLong);
         subtitle.startAnimation(moveUp);
 
+    }
+
+    public void launchMenuActivity(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
