b0ef85d1d67d1774b1f559f90188f9f334708c5d
holmestrevor <holmes.trevor98@gmail.com>
2019-02-28 16:17:33 -0500

    Began adding animations to MenuActivity.

--- app/src/main/java/com/example/portfolioapp/MenuActivity.java
+++ app/src/main/java/com/example/portfolioapp/MenuActivity.java
@@ -3,22 +3,40 @@ package com.example.portfolioapp;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ListView;
+import android.widget.TextView;
+
 import com.example.menulistview.Category;
 import com.example.menulistview.CategoryAdapter;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
 
     private ArrayList<Category> categories;
+
+    TextView bioTitle;
+    ArrayList<TextView> bioParagraphs;
+    Animation fadeInRight, fadeInLeft;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        bioTitle = (TextView)findViewById(R.id.bioTitle);
+        bioParagraphs = new ArrayList<TextView>();
+        bioParagraphs.add((TextView)findViewById(R.id.bioParagraph1));
 
+        fadeInRight = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in_fromright);
+        fadeInLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in_fromleft);
+
+        bioTitle.setAnimation(fadeInRight);
 
         categories = generateCategories();
 
