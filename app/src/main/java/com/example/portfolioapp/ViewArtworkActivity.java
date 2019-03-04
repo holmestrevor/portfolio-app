28bc8e78f1b2c63acd558af0ee80ee6610b33e2a
holmestrevor <holmes.trevor98@gmail.com>
2019-03-04 10:30:59 -0500

    Added TouchImageView library for zooming of images.

--- app/src/main/java/com/example/portfolioapp/ViewArtworkActivity.java
+++ app/src/main/java/com/example/portfolioapp/ViewArtworkActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.ortiz.touchview.TouchImageView;
+
 public class ViewArtworkActivity extends AppCompatActivity {
 
     private ImageView mImageView;
+    //TouchImageView library provided courtesy of MikeOrtiz (https://github.com/MikeOrtiz/TouchImageView)
+    private TouchImageView test;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,17 @@ public class ViewArtworkActivity extends AppCompatActivity {
 
         Intent fromPrevious = getIntent();
 
-        mImageView = (ImageView) findViewById(R.id.artFull);
-        mImageView.setImageResource(fromPrevious.getIntExtra("imageID", R.drawable.poseidonbg));
+//        mImageView = (ImageView) findViewById(R.id.artFull);
+//        mImageView.setImageResource(fromPrevious.getIntExtra("imageID", R.drawable.poseidonbg));
+//
+//        if(mImageView.getDrawable().getIntrinsicWidth()>mImageView.getDrawable().getIntrinsicHeight()) {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        }
 
-        if(mImageView.getDrawable().getIntrinsicWidth()>mImageView.getDrawable().getIntrinsicHeight()) {
+        test = (TouchImageView) findViewById(R.id.artFull);
+        test.setImageResource(fromPrevious.getIntExtra("imageID", R.drawable.poseidonbg));
+
+        if(test.getDrawable().getIntrinsicWidth()>test.getDrawable().getIntrinsicHeight()) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
 
